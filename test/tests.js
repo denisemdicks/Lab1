d9951882df146fd64dd5ee4b6ecd7a404e485207
Denise Dicks <denisemdicks@gmail.com>
2019-04-24 05:51:24 -0700

    Declared const variable in tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,18 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('All incorrect answers is 0', (assert) => {
     //Arrange
+    
+    const nightActivityAnswer = 'night swimming';
+    const runLocationAnswer = 'woods';
+    const strangeFoodAnswer = 'dolmas';
+    const lookUpAnswer = 'star gazing';
+    const breakfastAnswer = 'yogurt parfait';
+    const expected = 0
+
     // Set up your parameters and expectations
+
+    if()
 
     //Act 
     // Call the function you're testing and set the result to a const
