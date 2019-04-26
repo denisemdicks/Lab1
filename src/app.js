c9e0891a1f74adac525a233bd919f2b15cf9bc50
Denise Dicks <denisemdicks@gmail.com>
2019-04-26 13:24:44 -0700

    Added document.getElementById to app.js.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import quizScore from './quizScore.js';
 const submitButton = document.getElementById('submit');
 //get all elements by id
+
+ const nightSwimming = document.getElementById('night swimming');
+ const woods = document.getElementById('woods');
+ const dolmas = document.getElementById('dolmas');
+ const stargazing = document.getElementById('stargazing');
+ const yogurtParfait = document.getElementById('yogurt-parfait');
 //add eventListener to button (const submitbutton.eventListener)
 
 submitButton.addEventListener('click', () => {
